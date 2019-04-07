0af04f8dadcf2a837c62bd8751f7644c4d30273b
fransiskusak <f.a.k@mail.ugm.ac.id>
2019-04-07 12:38:31 +0700

    add RunWith, SerenityRunner, and @Test    rev

--- src/test/java/com/mycompany/elok/steps/LogoutScenario.java
+++ src/test/java/com/mycompany/elok/steps/LogoutScenario.java
@@ -1,20 +1,27 @@
 package com.mycompany.elok.steps;
 
 import com.mycompany.elok.steps.serenity.EndUserSteps;
+import net.serenitybdd.jbehave.SerenityJBehaveTestRunner;
 import net.thucydides.core.annotations.Steps;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by Fransiskus A K on 19/03/2019.
  */
+
+//////////////////////////////////////////Konfig dari github//////////////////
+
+@RunWith(SerenityJBehaveTestRunner.class)
 public class LogoutScenario {
     @Steps
     EndUserSteps endUser;
 
     // ///////////////////////LOGOUT//////////////////////////////////////////
-
+    @Test
     @Given("Pengguna berada pada Halaman Dashboard e-LOK")
     public void penggunaBeradaPadaHalamanDashboard() {endUser.beradaDiDashboardpage();}
 
